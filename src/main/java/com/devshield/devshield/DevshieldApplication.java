86ecc80ce6a08fff712391ba13a4a670bf1ad1f3
Quan Tran <thequantran2005@gmail.com>
2025-12-02 02:46:45 -0500

    connected to database

---
+++ src/main/java/com/devshield/devshield/DevshieldApplication.java
@@ -0,0 +1,13 @@
+package com.devshield.devshield;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevshieldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevshieldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devshield/devshield/DevshieldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devshield.devshield;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevshieldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
